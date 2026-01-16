8c5747e508fe81af65f5c61b224f95cf749e0939
IlyaStudent <ilyaprepodavatel@gmail.com>
2026-01-16 04:49:50 +0300

    add custom exceptions and exception handler

---
+++ src/main/java/com/example/bankcards/util/ApiConstants.java
@@ -0,0 +1,14 @@
+package com.example.bankcards.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiConstants {
+
+    public static final String UNDEFINED = "undefined";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+    public static final String BREAK_LINE = "\n";
+    public static final String BASE_PACKAGE = "com.example.bankcards";
+}
