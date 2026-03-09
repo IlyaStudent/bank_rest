235ed527ba605fbd461092f7ab6b2409b73b5e47
IlyaStudent <ilyaprepodavatel@gmail.com>
2026-03-09 18:12:35 +0300

    n+1 problem

--- src/main/java/com/example/bankcards/repository/UserRepository.java
+++ src/main/java/com/example/bankcards/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.example.bankcards.repository;
 
 import com.example.bankcards.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 
 import java.util.Optional;
 
@@ -12,4 +16,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByUsername(String username);
 
     Boolean existsByEmail(String email);
+
+    @EntityGraph(attributePaths = "roles")
+    @NonNull
+    Page<User> findAll(@NonNull Pageable pageable);
+
 }
